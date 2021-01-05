44035135285dc1fda977bf5e5405f6167c361d88
AndreLeEEEEE <72272834+AndreLeEEEEE@users.noreply.github.com>
2021-01-05 07:14:27 -0700

    Update Plex2Board.py

--- Plex2Board.py
+++ Plex2Board.py
@@ -218,7 +218,7 @@ def setup_plex(remote):
     """Get plex ready for use."""
     remote.get("https://www.plexonline.com/modules/systemadministration/login/index.aspx?")
     remote.find_element_by_name("txtUserID").send_keys("w.Andre.Le")
-    remote.find_element_by_name("txtPassword").send_keys("ThisExpires7")
+    remote.find_element_by_name("txtPassword").send_keys("OokyOoki2")
     remote.find_element_by_name("txtCompanyCode").send_keys("wanco")
     locate_by_id(remote, "btnLogin")
     remote.switch_to.window(remote.window_handles[1])
